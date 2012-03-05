06ec79acae90c46ff25cf077fe584411584a4e19
JamesOravec <James.Oravec@gmail.com>
2012-03-04 21:01:53 -0800

    Got example code working with crawler4j-3.3
    Verified downloading images properly.
    
    Our program will use crawler4j-3.3 for the spidering part.
    
    The code will have to be modify to work in the way we desire.
    
    Note:
    To run image crawler example
    	edu.uci.ics.crawler4j.examples.imagecrawler.ImageCrawlController
    		arguments used: root 1 images
    Config information is stored in "root" and images save to "images"

---
+++ examples/com/jamesoravec/sample/spider/HTMLParse.java
@@ -0,0 +1,10 @@
+package com.jamesoravec.sample.spider;
+
+import javax.swing.text.html.*;
+
+public class HTMLParse extends HTMLEditorKit {
+
+	public HTMLEditorKit.Parser getParser() {
+		return super.getParser();
+	}
+}
---
+++ examples/com/jamesoravec/sample/spider/ISpiderReportable.java
@@ -0,0 +1,11 @@
+package com.jamesoravec.sample.spider;
+
+import java.net.*;
+
+interface ISpiderReportable {
+	public boolean spiderFoundURL(URL base, URL url);
+
+	public void spiderURLError(URL url);
+
+	public void spiderFoundEMail(String email);
+}
